c64e087bb905248c9cdc63818b8cf88d38b63bf8
Nevyn Bengtsson <nevyn@alloverse.com>
2022-11-03 22:57:24 +0100

    rewrite it to be more like an app dock

--- lua/main.lua
+++ lua/main.lua
@@ -1,9 +1,11 @@
+local PlaceManager = require("placemanager")
+
 local client = Client(
     arg[2], 
     "placesettings"
 )
 
-local app = App(client)
+app = App(client)
 assets = {
     quit = ui.Asset.File("images/quit.png"),
     app = ui.Asset.File("images/app.png"),
@@ -12,146 +14,63 @@ assets = {
 app.assetManager:add(assets)
 
 local root = ui.View(
-    ui.Bounds(0,1.5,0,   0.8, 1, 0.01)
-        :rotate(-3.14/2, 0,1,0)
-        :move(-5,0,2)
+    ui.Bounds(0,0.8,0,   0.8, 0.5, 0.01)
+        :rotate(3.14/2, 0,1,0)
+        
+        :move(-2.6, 0, 0.5)
 )
 root.grabbable = true
 
-local mainView = root:addSubview(ui.Surface(
-    ui.Bounds(0,0,0,   0.8, 0.5, 0.01)
-))
-mainView:setColor({0.7,0.7,0.7,1})
-
-local heading = mainView:addSubview(ui.Label{
-    bounds= ui.Bounds(0,0,0,  mainView.bounds.size.width, 0.05, 0.01),
-    halign= "left"
-})
-heading:doWhenAwake(function()
-    heading:setText("Admin: "..app.client.placename)
-end)
-
-local stack = nil
-local admindata = nil
---  {
---      display_name= "Decorator",
---      is_visor= false,
---      connected_at= 1643407779.0,
---      agent_id= "123",
---      stats= "asdf"
---  }
-local agentToRowMap = {}
-
-function setAdminData(data)
-    if admindata and data and tablex.deepcompare(data, admindata) then return end
-    admindata = data
-
-    local c = #data
-    local rowHeight = 0.06
-    local rowMargin = 0.005
-    local dataHeight = c*rowHeight
-    local totalHeight = (rowHeight+rowMargin)*c + 0.08
-    local w = mainView.bounds.size.width
-    mainView.bounds.size.height = totalHeight
-    mainView:setBounds()
-
-    heading.bounds:moveToOrigin():move(mainView.bounds.size:getEdge("top", "center")):move(0.03, -0.03, 0)
-    heading:setBounds()
-
-    local stackBounds = ui.Bounds(0,-heading.bounds.size.height/2,0, w, dataHeight, 0.01)
-    if not stack then
-        stack = mainView:addSubview(ui.StackView(stackBounds, "vert"))
-        stack:margin(rowMargin)
-    else
-        stack:setBounds(stackBounds)
-    end
-
-    for _, client in ipairs(data) do
-        local row = agentToRowMap[client.agent_id]
-        if not row then
-            row = stack:addSubview(ui.View())
-            agentToRowMap[client.agent_id] = row
-        end
-        row:setBounds(ui.Bounds(0,0,0, w, rowHeight, 0.01))
-        
-        row.label = row.label and row.label or row:addSubview(ui.Label{
-            halign= "left"
-        })
-        row.label:setBounds(row.bounds:copy():insetEdges(0.1, 0, 0.01, 0.01, 0, 0))
-        row.label:setText(client.display_name)
-        
-        
-        row.icon = row.icon and row.icon or row:addSubview(ui.Surface())
-        row.icon:setBounds(row.bounds:copy():insetEdges(0.030, w-rowHeight-0.015, 0.01, 0.01, 0, 0.001))
-        row.icon:setTexture(client.is_visor and assets.person or assets.app)
-        
-        row.kickButton = row.kickButton and row.kickButton or row:addSubview(ui.Button())
-        row.kickButton:setBounds(row.bounds:copy():insetEdges(w/1.3, 0.04, 0.01, 0.01, -0.02, 0.006))
-        
-        row.kickButton.label:setText(client.is_visor and "Kick" or "Quit")
-        row.kickButton.onActivated = function()
-            print("kicking app", pretty.write(client))
-            app.client:sendInteraction({
-                sender_entity_id = mainView.entity.id,
-                receiver_entity_id = "place",
-                body = {
-                    "kick_agent",
-                    client.agent_id
-                }
-            }, function(resp, body)
-                if body[2] ~= "ok" then
-                    print("not kicking:", body[3])
-                    ui.StandardAnimations.addFailureAnimation(row.kickButton, 0.03)
-                    return
-                end
-                print("ok!")
-                fetchAdminData()
-            end)
-        end
-        row.statsLabel = row.statsLabel and row.statsLabel or row:addSubview(ui.Label{
-            wrap= true,
-            halign= "left"
-        })
-        row.statsLabel:setBounds(row.bounds:copy():insetEdges(w/1.8, row.kickButton.bounds:getEdge("left"), 0.025, 0.024, -0.02, 0.006))
-        row.statsLabel:setText(client.stats)
-    end
-
-    for agent_id, row in pairs(agentToRowMap) do
-        local found = false
-        for _, client in ipairs(data) do
-            if client.agent_id == agent_id then
-                found = true
-            end
-        end
-        if not found then
-            row:removeFromSuperview()
-            agentToRowMap[agent_id] = nil
-        end
-    end
-
-    stack:layout()
-end
-
-function fetchAdminData()
-    app.client:sendInteraction({
-        sender_entity_id = mainView.entity.id,
-        receiver_entity_id = "place",
-        body = {
-            "list_agents"
-        }
-    }, function(resp, body)
-        if body[1] ~= "list_agents" and body[2] ~= "ok" then return end
-        setAdminData(body[3])
-    end)
-end
-
-setAdminData({})
-mainView:doWhenAwake(function()
+local manager = PlaceManager()
+root:doWhenAwake(function()
     app:scheduleAction(1, true, function()
-        fetchAdminData()
+        manager:refresh()
     end)
-    
 end)
+
+
+local dockView = root:addSubview(ui.StackView(
+    ui.Bounds(0,0,0,   0.4, 0.4, 0.01)
+        :rotate(-3.14/8, 1,0,0),
+    "horizontal"
+))
+
+--local heading = dockView:addSubview(ui.Label{
+--    bounds= ui.Bounds(0,-root.bounds.size.height/3,0,  dockView.bounds.size.width, 0.05, 0.01),
+--    halign= "center",
+--})
+--heading:doWhenAwake(function()
+--    heading:setText(app.client.placename)
+--end)
+
+local appDock = dockView:addSubview(ui.StackView(
+    ui.Bounds(0,0,0, 0.4, 0.4, 0.01),
+    "horizontal"
+))
+local userDock = dockView:addSubview(ui.StackView(
+    ui.Bounds(0,0,0, 0.4, 0.4, 0.01),
+    "horizontal"
+))
+
+
+class.AgentView(ui.Button)
+function AgentView:_init(agent)
+    self:super(ui.Bounds(0,0,0,  0.1, 0.15, 0.05))
+    self.agent = agent
+    self.label.text = agent.display_name
+    self.defaultTexture = agent.is_visor and assets.person or assets.app
+end
+
+manager.onAppAdded = function(agent)
+    local button = AgentView(agent)
+    appDock:addSubview(button)
+    dockView:layout()
+end
+manager.onUserAdded = function(agent)
+    local button = AgentView(agent)
+    userDock:addSubview(button)
+    dockView:layout()
+end
 
 app.mainView = root
 
---
+++ lua/placemanager.lua
@@ -0,0 +1,120 @@
+class.PlaceManager()
+
+--  {
+--      display_name= "Decorator",
+--      is_visor= false,
+--      connected_at= 1643407779.0,
+--      agent_id= "123",
+--      avatar_id= "123",
+--      stats= "asdf"
+--  }
+
+function PlaceManager:_init()
+    self.agents = {}
+    self.apps = {}
+    self.users = {}
+
+    self.onAgentAdded = function(agent) end
+    self.onAgentUpdated = function(agent) end
+    self.onAgentRemoved = function(agent) end
+    self.onUserAdded = function(agent) end
+    self.onUserUpdated = function(agent) end
+    self.onUserRemoved = function(agent) end
+    self.onAppAdded = function(agent) end
+    self.onAppUpdated = function(agent) end
+    self.onAppRemoved = function(agent) end
+end
+
+function PlaceManager:refresh()
+    app.client:sendInteraction({
+        receiver_entity_id = "place",
+        body = {
+            "list_agents"
+        }
+    }, function(resp, body)
+        if body[1] ~= "list_agents" and body[2] ~= "ok" then return end
+        self:_setData(body[3])
+    end)
+end
+
+function PlaceManager:getAgent(aid)
+    return self.agents[aid]
+end
+
+function PlaceManager:_setData(data)
+    local newAgents = {}
+    for _, agent in ipairs(data) do
+        local existing = self:getAgent(agent.agent_id)
+        newAgents[agent.agent_id] = agent
+        if not existing then
+            setmetatable(agent, PlaceAgent)
+            self.agents[agent.agent_id] = agent
+            self.onAgentAdded(agent)
+            if agent.is_visor then
+                self.users[agent.agent_id] = agent
+                self.onUserAdded(agent)
+            else
+                self.apps[agent.agent_id] = agent
+                self.onAppAdded(agent)
+            end
+        else
+            tablex.update(existing, agent)
+            self.onAgentUpdated(agent)
+            if agent.is_visor then
+                self.onUserUpdated(agent)
+            else
+                self.onAppUpdated(agent)
+            end
+        end
+    end
+    for aid, agent in pairs(self.agents) do
+        local newAgent = newAgents[aid]
+        if not newAgent then
+            self.agents[aid] = nil
+            self.onAgentRemoved(agent)
+            if agent.is_visor then
+                self.users[aid] = nil
+                self.onUserRemoved(agent)
+            else
+                self.apps[aid] = nil
+                self.onAppRemoved(agent)
+            end
+        end
+    end
+end
+
+class.PlaceAgent()
+function PlaceAgent:quit(cb)
+    app.client:sendInteraction({
+        receiver_entity_id = self.agent_id,
+        body = {
+            "quit"
+        }
+    }, function(resp, body)
+        if body[2] ~= "ok" then
+            cb(false)
+        else
+            cb(true)
+            self:refresh()
+        end
+    end)
+end
+
+function PlaceAgent:kill(cb)
+    app.client:sendInteraction({
+        receiver_entity_id = "place",
+        body = {
+            "kick_agent",
+            client.agent_id
+        }
+    }, function(resp, body)
+        if body[2] ~= "ok" then
+            cb(false)
+        else
+            cb(true)
+            self:refresh()
+        end
+    end)
+end
+
+return PlaceManager
